09d149bdf0cc48bf24fd852aa37e22edd6cebb9e
William Andrey Silva <46973365+Will5507@users.noreply.github.com>
2022-08-27 21:34:50 -0300

    1

---
+++ assets/js/data/firebase.js
@@ -0,0 +1,18 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyDEUgIu5CBj7lYWOCXD3TK4jY7WGM16iwc",
+   authDomain: "semana-mundo-invertido-30262.firebaseapp.com",
+   projectId: "semana-mundo-invertido-30262",
+   storageBucket: "semana-mundo-invertido-30262.appspot.com",
+   messagingSenderId: "735287524268",
+   appId: "1:735287524268:web:a9b2d120e55d7decc5d809"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ export default app
